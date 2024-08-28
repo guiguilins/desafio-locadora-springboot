c16093a81853ebade88826d4af060962e4691aab
Victor <101398802+JoaoVictor031@users.noreply.github.com>
2024-08-28 09:23:13 -0300

    Add files via upload

---
+++ desafio-locadora/desafio-locadora/src/test/java/com/example/desafio_locadora/DesafioLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
