712e537729107d6f76530d4738537634959e60cb
unknown <danisantos5075@gmail.com>
2024-08-28 17:18:51 -0300

    feat: Criando FuncionarioModel + validation dependency

---
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/domain/entity/FuncionarioModel.java
@@ -0,0 +1,17 @@
+package com.example.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(name = "funcionario")
+public class FuncionarioModel extends PessoaModel {
+
+    @NotBlank
+    private String matricula;
+}
