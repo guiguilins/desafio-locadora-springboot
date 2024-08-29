b6bf4e9d4f9ee56baf4cf918e3471572f205b4a1
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-08-29 18:07:03 -0300

    fix: dto motorista

--- desafio-locadora/desafio-locadora/src/main/java/com/example/api/dtos/MotoristaDTO.java
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/api/dtos/MotoristaDTO.java
@@ -1,12 +1,13 @@
 package com.example.api.dtos;
 
+import com.example.domain.entity.MotoristaModel;
 import com.example.domain.entity.PessoaModel;
 import com.example.domain.enums.Sexo;
 
 import java.util.Date;
 
-public record MotoristaDTO(Long id, String nome, String cpf, Date dataNascimento, Sexo sexo) {
-    public MotoristaDTO(PessoaModel pessoa){
-        this(pessoa.getId(), pessoa.getNome(), pessoa.getCpf(),pessoa.getDataNascimente(),pessoa.getSexo());
+public record MotoristaDTO(Long id, String nome, String cpf, Date dataNascimento, Sexo sexo, String cnh) {
+    public MotoristaDTO(PessoaModel pessoa,String cnh){
+        this(pessoa.getId(), pessoa.getNome(), pessoa.getCpf(),pessoa.getDataNascimento(),pessoa.getSexo(),cnh);
     }
 }
