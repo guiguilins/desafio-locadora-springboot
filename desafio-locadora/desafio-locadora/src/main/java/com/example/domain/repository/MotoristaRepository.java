a338df17f0a4e0d4ae29ec8001e91ec5f58b6893
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-08-29 19:44:17 -0300

    feat: create Object interface to implement other servicesImpl

---
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/domain/repository/MotoristaRepository.java
@@ -0,0 +1,7 @@
+package com.example.domain.repository;
+
+import com.example.domain.entity.MotoristaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotoristaRepository extends JpaRepository<MotoristaModel,Long> {
+}
