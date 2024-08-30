5722de07c1391f545c7fece713042098fbe313a4
JoaoVictor031 <joaovictormeira13@gmail.com>
2024-08-30 00:05:01 -0300

    Criação: Mapper do ApoliceSeguro

---
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/api/mapper/ApoliceSguroMapper.java
@@ -0,0 +1,19 @@
+package com.example.api.mapper;
+
+import com.example.api.dtos.ApoliceSeguroDTO;
+import com.example.domain.entity.ApoliceSeguroModel;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApoliceSguroMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ApoliceSeguroDTO convertToApoliceSeguroDTO(ApoliceSeguroModel apoliceSeguroModel) {
+        return modelMapper.map(apoliceSeguroModel, ApoliceSeguroDTO.class);
+    }
+}
