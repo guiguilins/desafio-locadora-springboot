b9a4eff4a3a3da350ee0e14577e0b2ffc942378e
guiguilins <guiguilins56@gmail.com>
2024-08-31 11:42:22 -0300

    feat: implementacao do filtro de carros na camada de servico repository utilizando JPQL e controller

--- desafio-locadora/desafio-locadora/src/main/java/com/example/domain/repository/CarroRepository.java
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/domain/repository/CarroRepository.java
@@ -1,9 +1,19 @@
 package com.example.domain.repository;
 
 import com.example.domain.entity.CarroModel;
+import com.example.domain.enums.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CarroRepository extends JpaRepository<CarroModel,Long> {
+
+    List<CarroModel> findByModeloCategoria(Categoria categoria);
+
+    @Query("SELECT c FROM CarroModel c JOIN c.acessorios a WHERE a.id IN :acessoriosIds GROUP BY c HAVING COUNT(DISTINCT a.id) = :acessoriosCount")
+    List<CarroModel> findByAcessorios(@Param("acessoriosIds") List<Long> acessoriosIds, @Param("acessoriosCount") long acessoriosCount);
 }
--- desafio-locadora/desafio-locadora/src/main/java/com/example/domain/service/CarroService.java
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/domain/service/CarroService.java
@@ -3,6 +3,7 @@ package com.example.domain.service;
 import com.example.api.dtos.CarroDTO;
 import com.example.api.dtos.CarroRequestDTO;
 import com.example.domain.entity.CarroModel;
+import com.example.domain.enums.Categoria;
 
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface CarroService {
     public List<CarroDTO> listarCarros();
 
     public CarroModel salvarCarro(CarroRequestDTO data);
+
+    public List<CarroModel> filtrarCarros(Categoria categoria, List<Long> acessoriosIds);
 }
