23d0d122a462c1dec70157b44ba68dd86ea04b63
guiguilins <guiguilins56@gmail.com>
2024-09-02 06:11:21 -0300

    fix: removed databaseconfig for code run

--- desafio-locadora/desafio-locadora/src/main/java/com/example/domain/entity/ApoliceSeguroModel.java
+++ desafio-locadora/desafio-locadora/src/main/java/com/example/domain/entity/ApoliceSeguroModel.java
@@ -4,38 +4,31 @@ import java.math.BigDecimal;
 
 import com.example.api.dtos.ApoliceSeguroRequestDTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "apolice_seguro")
 public class ApoliceSeguroModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @NotNull
     private BigDecimal valorFranquia;
 
     @NotNull
-    private Boolean protecaoTerceiro;
+    private boolean protecaoTerceiro;
 
     @NotNull
-    private Boolean protecaoCausasNaturais;
+    private boolean protecaoCausasNaturais;
 
     @NotNull
-    private Boolean protecaoRoubo;
+    private boolean protecaoRoubo;
 
     @OneToOne(mappedBy = "apolice")
     @JsonIgnore
@@ -46,12 +39,5 @@ public class ApoliceSeguroModel {
         this.protecaoTerceiro = data.protecaoTerceiro();
         this.protecaoCausasNaturais = data.protecaoCausasNaturais();
         this.protecaoRoubo = data.protecaoRoubo();
-    }
-    
-    public ApoliceSeguroModel(BigDecimal valorFranquia, Boolean protecaoTerceiro, Boolean protecaoCausasNaturais, Boolean protecaoRoubo) {
-        this.valorFranquia = valorFranquia;
-        this.protecaoTerceiro = protecaoTerceiro;
-        this.protecaoCausasNaturais = protecaoCausasNaturais;
-        this.protecaoRoubo = protecaoRoubo;
     }
 }
